9536ee7c3c6c3504920390df595a0a9d618ebd05
alex <nemesis0288@gmail.com>
2025-02-17 08:30:06 -0800

    Add files via upload

---
+++ List.lua
@@ -0,0 +1,35 @@
+Asphalt
+Basalt
+Brick
+Diamond Plate
+Fabric
+Foil
+Cobblestone Forcefield
+Concrete Corroded Metal
+Cracked Lava
+Glacier
+Glass
+Granite
+Grass
+Ground
+Ice
+Leafy Grass
+Limestone
+Marble
+Metal
+Mud
+Neon
+Pavement
+Pebble
+Plastic
+Rock
+Salt
+Sand
+Sandstone
+Slate
+Smooth Plastic
+Snow
+Wood
+Wood Planks
+
+-- credits to farzad lol
